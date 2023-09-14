335dfabb7bba6dc04a2480fc7effb67a302ced62
Millianen <www.milli.matti@gmail.com>
2023-09-13 21:45:23 -0300

    Tarefa.teste

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Tipo from '../src/sections/tipo/Tipo'
+import Tarefa from '../src/sections/tarefa/Tarefa'
+
+
+function App() {
+  return (
+    <div className="container">
+      <Tipo />        
+      <Tarefa />        
+    </div>
+  );
+}
+
+export default App;
